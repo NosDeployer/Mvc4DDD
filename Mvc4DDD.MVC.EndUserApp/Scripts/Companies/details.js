9ef4e5e9865ea381d85428809fa8ab6d0097de2e
gustavofrizzo <guto.smo@gmail.com>
2015-01-15 04:54:03 -0200

    Improving the User interface.

--- Mvc4DDD.MVC.EndUserApp/Scripts/Companies/details.js
+++ Mvc4DDD.MVC.EndUserApp/Scripts/Companies/details.js
@@ -16,11 +16,61 @@
     marker.setMap(map);
 }
 
+function friendlyPhones(s) {
+    if (s.trim() === "-" || !s) return s;
+
+    var list = s.split(";");
+    for (var i = 0; i < list.length; i++) {
+        list[i] = "+" + list[i];
+    }
+
+    return list.join("<br/>");
+}
+
+function friendlyOp(s) {
+    if (s.trim() === "-" || !s) return s;
+
+    var list = s.split(";");
+    for (var i = 0; i < list.length; i++) {
+        var day = list[i].trim()[0];
+        list[i] = list[i].replace(day, codeToDay(day));
+    }
+
+    return list.join("<br/>");
+}
+
+function codeToDay(code) {
+    switch (code) {
+    case "E":
+        return "Monday";
+    case "T":
+        return "Tuesday";
+    case "K":
+        return "Wednesday";
+    case "N":
+        return "Thursday";
+    case "R":
+        return "Friday";
+    case "L":
+        return "Saturday";
+    case "P":
+        return "Sunday";
+    default:
+        return "Unknown";
+    }
+}
+    
+
 $(document).ready(function () {
 
     $.ajaxSetup({
         cache: false
     });
+
+    $("#addPhones").html(friendlyPhones($("#addPhones").text()));
+    $("#mainPhone").html(friendlyPhones($("#mainPhone").text()));
+    $("#fax").html(friendlyPhones($("#fax").text()));
+    $("#openingH").html(friendlyOp($("#openingH").text()));
 
     $('#mapButton').click(function () {
 
